bbc2aac3d6438c9b97a85518af21ace78d609736
luferrduc <lu.ferrando@duocuc.cl>
2024-11-21 19:57:59 -0300

    Add: GifItem test suite

--- tests/components/GiftItem.test.tsx
+++ tests/components/GiftItem.test.tsx
@@ -1,19 +1,38 @@
 import React from 'react'
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components"
-// import { GifItem } from '@co'
 
 describe('Se prueba <GifItem />', () => { 
   
-  const images = {
+  const image = {
     id: "",
     title: "Dragon Ball Gif",
     url: "https://giphy.com/gifs/dragon_ball.gif"
   }
 
+  const { id, title, url } = image
+
   test('debe hacer match con el snapshot', () => { 
-      const { title, url } = images
-      const { container } = render(<GifItem key={images.id} title={title} url={url} />)
+      
+      const { container } = render(<GifItem key={id} title={title} url={url} />)
       expect(container).toMatchSnapshot()
+  })
+
+  test('debe de mostrar la imagen con el URL y el ALT indicado', () => { 
+    
+    render(<GifItem title={title} url={url} />)
+
+    // expect(screen.getByRole('img').src).toBe(url)
+    // expect(screen.getByRole('img').alt).toBe(title)
+    const { src, alt } = screen.getByRole('img') as HTMLImageElement;
+    expect(src).toBe(url)
+    expect(alt).toBe(title)
+  })
+
+  test('debe de mostrar el título entregado en las props', () => { 
+    render(<GifItem title={title} url={url} />)
+    
+    expect(screen.getByText(title)).toBeTruthy()
+
   })
 })
